7680221c7f2f41276974f8e7c2ba4510a7d97d40
unknown <swathytech@outlook.com>
2019-06-20 13:26:06 -0400

    added route path to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import {Routes,RouterModule } from '@angular/router';
 
-
+//Importing components
+import { SimpleLayoutComponent } from './layouts/simple-layout/simple-layout.component';
+import {LoginComponent } from './auth/login/login.component';
  
 
 export const routes:Routes =[
 
     {
-
+        path:'',
+        component:SimpleLayoutComponent,
+        data:{
+            title:'Auth'
+        },
+        children:[
+            {
+                path:'auth/login',
+                component:LoginComponent
+            }
+        ]
     }
 
 
