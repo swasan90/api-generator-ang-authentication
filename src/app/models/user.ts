2f6fe7715332508e8ae266cecad678e16dfed243
unknown <swathytech@outlook.com>
2019-06-20 13:25:43 -0400

    created models folder and created user class to define metadata

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    id:number;
+    firstName: String;
+    lastName: String;
+    email:String;
+    password:String;
+    confirmPassword:String;
+    uuid:String;
+    createdAt:Date;
+    updatedAt:Date;
+    status:boolean;
+    enabled:boolean;
+}
