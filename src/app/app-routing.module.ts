b83b5a266578be169096f712e56a5e6f9033b66e
unknown <swathytech@outlook.com>
2019-06-20 14:43:41 -0400

    added path to forgot password route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { NgModule } from '@angular/core';
 import {Routes,RouterModule } from '@angular/router';
 
@@ -28,9 +29,14 @@ export const routes:Routes =[
             {
                 path:'login',
                 component:LoginComponent
+            },
+            {
+                path:'forgot_password',
+                component:ForgotPasswordComponent
             }
         ]
-    }
+    },
+    
 
 
 
