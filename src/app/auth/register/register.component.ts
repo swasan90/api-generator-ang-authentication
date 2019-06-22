554e5a34f236b12983ad3701c90f326dab18e7da
unknown <swathytech@outlook.com>
2019-06-22 00:37:56 -0400

    formatted register with comments

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,3 +1,6 @@
+/**
+ * @author Swathy Santhoshkumar
+ */
 import { Component, OnInit } from '@angular/core';
 import { User } from 'app/models/user';
 import { RegisterService } from './register.service';
@@ -7,39 +10,45 @@ import { NgForm } from '@angular/forms';
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers:[RegisterService]
+  providers: [RegisterService]
 })
+/**
+ * Class to implement register component methods.
+ */
 export class RegisterComponent implements OnInit {
 
-  constructor(private registerService:RegisterService) { }
-  error:String;
-  user:User;
-  showError:boolean=false;
-  message:String;
-  isSuccess:boolean=false;
-
-  register(registerRequestForm:NgForm){
-    let userObj:any ={};
+  constructor(private registerService: RegisterService) { }
+  error: String;
+  user: User;
+  showError: boolean = false;
+  message: String;
+  isSuccess: boolean = false;
+  /**
+   * Function to register user which calls the register service method.
+   * @param registerRequestForm 
+   */
+  register(registerRequestForm: NgForm) {
+    let userObj: any = {};
     userObj.firstName = this.user.firstName;
     userObj.lastName = this.user.lastName;
     userObj.email = this.user.email;
     userObj.password = this.user.password;
     userObj.confirmPassword = this.user.confirmPassword;
-    this.registerService.registerUser(userObj).subscribe(data=>{
+    this.registerService.registerUser(userObj).subscribe(data => {
       this.isSuccess = true;
       this.message = data["message"];
-    },error=>{
-        this.showError =true;
-        this.error  ="";         
-        if(error.error["errors"]){
-          let errorObj = error.error["errors"];
-          for(let obj of errorObj){
-            this.error += obj.defaultMessage+" \n";
-          }
-        }else{
-          this.error =error.error.message;
+    }, error => {
+      this.showError = true;
+      this.error = "";
+      if (error.error["errors"]) {
+        let errorObj = error.error["errors"];
+        for (let obj of errorObj) {
+          this.error += obj.defaultMessage + " \n";
         }
-      });
+      } else {
+        this.error = error.error.message;
+      }
+    });
   }
 
   ngOnInit() {
--- src/app/auth/register/register.service.ts
+++ src/app/auth/register/register.service.ts
@@ -1,3 +1,6 @@
+/**
+ * @author Swathy Santhoshkumar
+ */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'app/models/user';
@@ -7,11 +10,19 @@ import { environment } from 'environments/environment';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * Class to implement register service methods.
+ */
 export class RegisterService {
 
-  constructor(private httpClient:HttpClient) {  }
+  constructor(private httpClient: HttpClient) { }
 
-  public registerUser(userObj:User):Observable<User>{
-    return this.httpClient.post<User>(environment.api_url + "register",userObj);
+  /**
+   * Function to implement register user method.
+   * @param userObj 
+   */
+  public registerUser(userObj: User): Observable<User> {
+    return this.httpClient.post<User>(environment.api_url + "register", userObj);
   }
 }
