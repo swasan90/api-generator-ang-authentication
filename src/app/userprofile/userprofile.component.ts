56f728d780f8b976d775c8928f5f7dc32577944d
unknown <swathytech@outlook.com>
2019-06-22 00:13:02 -0400

    completed user profile feature to show user's profile data

---
+++ src/app/userprofile/userprofile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'app/models/user';
+
+@Component({
+  selector: 'app-userprofile',
+  templateUrl: './userprofile.component.html',
+  styleUrls: ['./userprofile.component.css']
+})
+export class UserprofileComponent implements OnInit {
+
+  user:User;
+  constructor() { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem("currentUser"));    
+  }
+
+}
