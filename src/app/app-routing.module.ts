8ca545a0470c16738d4e32dbd594f869ddb8dc05
unknown <swathytech@outlook.com>
2019-06-22 00:16:46 -0400

    added routing paths for logout and my profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { AuthGuard } from './auth/auth-guard';
+import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { NgModule } from '@angular/core';
 import {Routes,RouterModule } from '@angular/router';
@@ -8,6 +11,7 @@ import {LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AccountActivationComponent } from './auth/account-activation/account-activation.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { LogOutComponent } from './auth/logout.component';
  
  
 
@@ -44,15 +48,33 @@ export const routes:Routes =[
         component:SimpleLayoutComponent,
         children:[
             {
-                path:'activate',
+                path:'activate?token=',
                 component:AccountActivationComponent
             },
             {
-                path:'reset',
+                path:'reset?token=',
                 component:ResetPasswordComponent
             }
         ]
-    } 
+    },
+    {
+        path:'home',
+        component:FullLayoutComponent,
+        canActivate:[AuthGuard],
+        data:{
+            title:'Home',
+        },
+        children:[
+            {
+                path:'user_profile',
+                component: UserprofileComponent 
+            },
+            {
+                path:'auth/logout',
+                component: LogOutComponent
+            }
+        ]
+    }
 ];
 
 @NgModule({
