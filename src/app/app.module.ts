9bccc02432530843f220feea5060c70c17ea6e51
unknown <swathytech@outlook.com>
2019-06-22 00:17:06 -0400

    added necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppComponent } from './app.component';
+
 
  //Routing Module
 import {AppRoutingModule} from './app-routing.module';
+//Importing components
 import { SimpleLayoutComponent } from './layouts/simple-layout/simple-layout.component';
 import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -15,7 +16,17 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { AccountActivationComponent } from './auth/account-activation/account-activation.component'; 
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { AppComponent } from './app.component';
 
+import { JwtInterceptor } from './auth/jwt-interceptor';
+import { ErrorInterceptor } from './errorHandlers/error.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { LogOutComponent } from './auth/logout.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -26,16 +37,25 @@ import { ResetPasswordComponent } from './auth/reset-password/reset-password.com
     RegisterComponent,
     ForgotPasswordComponent,
     AccountActivationComponent,    
-    ResetPasswordComponent
+    ResetPasswordComponent, 
+    UserprofileComponent,
+    LogOutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['http://localhost:4200/'],         
+      }
+    })
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
