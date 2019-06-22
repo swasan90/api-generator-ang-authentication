a5e7d0bb4962793352176193fb815942893f6ec8
unknown <swathytech@outlook.com>
2019-06-22 00:11:36 -0400

    implemented auth guard to secure the routes

---
+++ src/app/auth/auth-guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({providedIn:'root'})
+
+export class AuthGuard implements CanActivate {
+
+    constructor(private router:Router,private authenticationService:AuthService){}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+        const currentUser = this.authenticationService.currentUserValue;
+        if(currentUser){
+            return true;
+        }
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
