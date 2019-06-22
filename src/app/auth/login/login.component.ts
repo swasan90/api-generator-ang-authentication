a4b0dd508946187975f2668785ac78fb07f17cbb
unknown <swathytech@outlook.com>
2019-06-22 00:34:46 -0400

    formatted login functionality files

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,43 +6,49 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  /**
+   * @author Swathy Santhoshkumar
+   */
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+/**
+ * Class to implement login component methods
+ */
 export class LoginComponent implements OnInit {
-  
 
-  user:User = new User();
-  message:string;
-  isError:boolean;
-  isForgot:boolean;
-  error:string;
-   
 
-  constructor(private router:Router,private authService:AuthService) { }
+  user: User = new User();
+  message: string;
+  isError: boolean;
+  isForgot: boolean;
+  error: string;
 
-   /**
-   * Function to authenticate user
-   * @param loginRequestForm 
-   */ 
-  authenticate(loginRequestForm: NgForm) {     
-    this.isError = false;  
-    this.authService.login(this.user).subscribe(currentUser => {      
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  /**
+  * Function to authenticate user
+  * @param loginRequestForm 
+  */
+  authenticate(loginRequestForm: NgForm) {
+    this.isError = false;
+    this.authService.login(this.user).subscribe(currentUser => {
       this.router.navigate(['/home/user_profile']);
     },
       error => {
-        loginRequestForm.controls['password'].reset();  
-        this.isError = true;  
-        this.error = error; 
+        loginRequestForm.controls['password'].reset();
+        this.isError = true;
+        this.error = error;
       });
   }
 
-  ngOnInit(): void {         
-    if( history.state.message){      
-        this.isForgot = true;
-        this.message = history.state.message;        
+  ngOnInit(): void {
+    if (history.state.message) {
+      this.isForgot = true;
+      this.message = history.state.message;
     }
-    
+
   }
 
 }
