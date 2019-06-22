b7e57fc72783b86597be43f7273c7963b84f147c
unknown <swathytech@outlook.com>
2019-06-22 00:45:50 -0400

    added comments

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,16 +1,24 @@
+/**
+ * @author Swathy Santhoshkumar
+ */
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 
 export class AuthGuard implements CanActivate {
 
-    constructor(private router:Router,private authenticationService:AuthService){}
+    constructor(private router: Router, private authenticationService: AuthService) { }
 
+    /**
+     * Function to implement canActivate function
+     * @param route 
+     * @param state 
+     */
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
         const currentUser = this.authenticationService.currentUserValue;
-        if(currentUser){
+        if (currentUser) {
             return true;
         }
         // not logged in so redirect to login page with the return url
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,6 @@
+/**
+ * @author Swathy Santhoshkumar
+ */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
@@ -10,56 +13,70 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * Auth service class
+ */
 export class AuthService {
 
-  private currentUserSubject:BehaviorSubject<User>;
-  public currentUser:Observable<User>;
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
 
-  constructor(private httpClient:HttpClient,private router:Router,private jwtHelper:JwtHelperService) { 
+  constructor(private httpClient: HttpClient, private router: Router, private jwtHelper: JwtHelperService) {
     this.currentUserSubject = new BehaviorSubject<User>(this.getUser());
     this.currentUser = this.currentUserSubject.asObservable();
 
   }
 
-
-  getUser(){
-    if(localStorage.getItem("currentUser")){
-      return JSON.parse(localStorage.getItem("currentUser"));      
+  /**
+   * Function to return the user from localstorage.
+   */
+  getUser() {
+    if (localStorage.getItem("currentUser")) {
+      return JSON.parse(localStorage.getItem("currentUser"));
     }
     return null;
   }
 
-  public get currentUserValue():User{
+  public get currentUserValue(): User {
     return this.currentUserSubject.value;
   }
 
-  login(requestPayload:any){
-    return this.httpClient.post<any>(environment.api_url+"login",requestPayload,{observe: 'response'}).pipe(map(resp=>{          
-        let token = resp.headers.get('Authorization').substring(7);  
-        if(token !=null){
-          localStorage.setItem('token',JSON.stringify(token));
-          localStorage.setItem('currentUser',JSON.stringify(this.jwtHelper.decodeToken(token)));          
-          this.currentUserSubject.next(this.getUser());               
-        }        
-        return this.getUser();
-    }),catchError(error=>{
+  /**
+   * Function to implement login functionality.
+   * @param requestPayload 
+   */
+  login(requestPayload: any) {
+    return this.httpClient.post<any>(environment.api_url + "login", requestPayload, { observe: 'response' }).pipe(map(resp => {
+      let token = resp.headers.get('Authorization').substring(7);
+      if (token != null) {
+        localStorage.setItem('token', JSON.stringify(token));
+        localStorage.setItem('currentUser', JSON.stringify(this.jwtHelper.decodeToken(token)));
+        this.currentUserSubject.next(this.getUser());
+      }
+      return this.getUser();
+    }), catchError(error => {
       return throwError(error);
     }));
   }
 
+  /**
+   * Function to implement logout functionality
+   */
+  logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("currentUser");
+    this.currentUserSubject.next(null);
+    this.router.navigate(['']);
+  }
+  /**
+   * Function to return if the user is logged in or not.
+   */
+  public get loggedIn(): boolean {
+    return localStorage.getItem('token') != null;
+  }
 
-logout(){
-   localStorage.removeItem("token");
-   localStorage.removeItem("currentUser");
-   this.currentUserSubject.next(null);
-   this.router.navigate(['']);
- }
 
- public get loggedIn(): boolean{
-  return localStorage.getItem('token') !=  null;
-}
-
- 
 
 }
