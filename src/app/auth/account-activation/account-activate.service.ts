14fb142681a6fed04aeb167894b0e69df5c1c4d0
unknown <swathytech@outlook.com>
2019-06-22 00:27:53 -0400

    added comments to account-activation module

--- src/app/auth/account-activation/account-activate.service.ts
+++ src/app/auth/account-activation/account-activate.service.ts
@@ -1,3 +1,6 @@
+/**
+ * @author Swathy SanthoshKumar
+ */
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -7,10 +10,17 @@ import { environment } from 'environments/environment';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Class to implement account activation methods
+ */
 export class AccountActivateService {
 
   constructor(private httpClient:HttpClient) { }
 
+  /**
+   * Function to activate the user.
+   * @param tokenStr 
+   */
   public activateUser(tokenStr:string):Observable<ResponseMessage>{
     const params = new HttpParams().set('token',tokenStr);   
     return this.httpClient.get<any>(environment.api_url+"activate", {params});
--- src/app/auth/account-activation/account-activation.component.ts
+++ src/app/auth/account-activation/account-activation.component.ts
@@ -2,13 +2,19 @@ import { ResponseMessage } from 'app/models/response-message';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AccountActivateService } from './account-activate.service';
-
+/**
+ * @author Swathy Santhoshkumar
+ */
 @Component({
   selector: 'app-account-activation',
   templateUrl: './account-activation.component.html',
   styleUrls: ['./account-activation.component.css'],
   providers:[AccountActivateService]
 })
+
+/**
+ * Class to implement account activation component methods
+ */
 export class AccountActivationComponent implements OnInit {
 
   constructor(private route:ActivatedRoute,private accountactivationService:AccountActivateService) { }
@@ -18,6 +24,9 @@ export class AccountActivationComponent implements OnInit {
   isError:boolean;
   message:string;  
 
+  /**
+   * Function to activate account.
+   */
   activateAccount(){
     this.accountactivationService.activateUser(this.param1).subscribe(data=>{ 
       this.message = data.message; 
@@ -26,7 +35,9 @@ export class AccountActivationComponent implements OnInit {
         this.message = error.error.message;
     })
   }
-
+  /**
+   * Function to get value of the queryparams from url on loading.
+   */
   ngOnInit() {
      this.route.queryParams.subscribe(params=>{
         this.param1 = params['token'];        
