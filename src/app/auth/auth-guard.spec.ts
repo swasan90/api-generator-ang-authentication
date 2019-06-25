03fd8f79471234acd95a6fd577f35898ae53da8b
unknown <swathytech@outlook.com>
2019-06-25 15:50:45 -0400

    fixed errors

--- src/app/auth/auth-guard.spec.ts
+++ src/app/auth/auth-guard.spec.ts
@@ -1,7 +1,15 @@
 import { AuthGuard } from './auth-guard';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('AuthGuard', () => {
+  let routerSpy: jasmine.SpyObj<Router>;
+  let authService:jasmine.SpyObj<AuthService>;
+
   it('should create an instance', () => {
-    expect(new AuthGuard()).toBeTruthy();
+    authService = TestBed.get(AuthService);
+    routerSpy = TestBed.get(Router);
+    expect(new AuthGuard(routerSpy,authService)).toBeTruthy();
   });
 });
