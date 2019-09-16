028ce2b705fb5834ae20fdbbdcf983be80b67caf
Swathy SanthoshKumar <swasan90@gmail.com>
2019-09-16 14:23:58 -0400

    updated env prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+   api_url: "http://localhost:8081/",
+  secret_key_uuid:"sECRETkEyuuid",
+  api_client_url:"http://localhost:4210/"
 };
