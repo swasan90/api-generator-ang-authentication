7bc01998c6a1fdda1e4a802e7e8bb3315cf3e87e
Swathy SanthoshKumar <swasan90@gmail.com>
2019-09-17 22:19:33 -0400

    added api auth url in environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-   api_url: "http://localhost:8081/",
-  secret_key_uuid:"sECRETkEyuuid",
-  api_client_url:"http://localhost:4210/"
+	api_url: "http://apigenerator-auth.us-east-1.elasticbeanstalk.com/",
+	secret_key_uuid:"sECRETkEyuuid",
+	api_client_url:"http://localhost:4210/"
 };
