d4e24cd1b6dff3b0fb6d67d586190e36dd14bd52
Swathy SanthoshKumar <swasan90@gmail.com>
2019-09-17 23:32:04 -0400

    added env to gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-	api_url: "http://apigenerator-auth.us-east-1.elasticbeanstalk.com/",
-	secret_key_uuid:"sECRETkEyuuid",
-	api_client_url:"http://localhost:4210/"
-};
